ce079a28cabce143c02160aab60cdb22f110338e
ruan-molinari <ruan.molinari@proton.me>
2025-07-21 10:32:02 -0300

    refactoring more

---
+++ src/http.rs
@@ -0,0 +1,127 @@
+pub enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    OPTIONS,
+}
+
+impl HttpMethod {
+    pub fn as_str(&self) -> &str {
+        match self {
+            HttpMethod::GET => "GET",
+            HttpMethod::POST => "POST",
+            HttpMethod::PUT => "PUT",
+            HttpMethod::DELETE => "DELETE",
+            HttpMethod::PATCH => "PATCH",
+            HttpMethod::OPTIONS => "OPTIONS",
+        }
+    }
+
+    pub fn from_str(method: &str) -> Option<HttpMethod> {
+        match method {
+            "GET" => Some(HttpMethod::GET),
+            "POST" => Some(HttpMethod::POST),
+            "PUT" => Some(HttpMethod::PUT),
+            "DELETE" => Some(HttpMethod::DELETE),
+            "PATCH" => Some(HttpMethod::PATCH),
+            "OPTIONS" => Some(HttpMethod::OPTIONS),
+            _ => None,
+        }
+    }
+}
+
+pub enum HttpStatus {
+    OK,
+    NotFound,
+    InternalServerError,
+    BadRequest,
+}
+
+impl HttpStatus {
+    pub fn as_str(&self) -> &str {
+        match self {
+            HttpStatus::OK => "200 OK",
+            HttpStatus::NotFound => "404 Not Found",
+            HttpStatus::InternalServerError => "500 Internal Server Error",
+            HttpStatus::BadRequest => "400 Bad Request",
+        }
+    }
+}
+
+pub struct HttpRequest {
+    pub method: HttpMethod,
+    pub target: String,
+    pub version: String,
+    pub headers: Vec<Header>,
+    pub body: Option<String>,
+}
+
+pub struct HttpResponse {
+    pub status: HttpStatus,
+    pub headers: Vec<Header>,
+    pub body: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Header {
+    pub key: String,
+    pub value: String,
+}
+
+impl HttpRequest {
+    pub fn new(buf: Vec<u8>) -> Self {
+        let request = String::from_utf8(buf).unwrap();
+        let mut lines = request.lines().collect::<Vec<_>>().into_iter();
+        let mut status_line = lines.next().unwrap().split_whitespace();
+
+        let mut headers = Vec::new();
+        while let Some(header_line) = lines.next() {
+            if let Some((key, value)) = header_line.split_once(':') {
+                headers.push(Header {
+                    key: key.trim().to_string(),
+                    value: value.trim().to_string(),
+                });
+            }
+        }
+        let body = String::from(lines.collect::<Vec<&str>>().concat());
+
+        let method =
+            HttpMethod::from_str(status_line.next().unwrap()).expect("Invalid HTTP method");
+
+        HttpRequest {
+            method: method,
+            target: status_line.next().unwrap().to_string(),
+            version: status_line.next().unwrap().to_string(),
+            headers: headers.to_vec(),
+            body: Some(body),
+        }
+    }
+}
+
+impl HttpResponse {
+    pub fn new(status: HttpStatus, headers: Vec<Header>, body: Option<String>) -> Self {
+        HttpResponse {
+            status,
+            headers,
+            body,
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut response = format!("{} {}\r\n", "HTTP/1.1", self.status.as_str());
+        for header in &self.headers {
+            response.push_str(&format!("{}: {}\r\n", header.key, header.value));
+        }
+        response.push_str("\r\n");
+        if let Some(body) = &self.body {
+            response.push_str(body);
+        }
+        response
+    }
+
+    pub fn as_bytes(&self) -> Vec<u8> {
+        self.to_string().into_bytes()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,12 @@
+mod http;
+
 use std::error::Error;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 
+use crate::http::{HttpRequest, HttpResponse, HttpStatus};
+
 const HTTP_VERSION: &str = "HTTP/1.1";
-
-struct HttpRequest {
-    method: String,
-    target: String,
-    version: String,
-    headers: Vec<Header>,
-    body: Option<String>,
-}
-
-#[derive(Debug, Clone)]
-struct Header {
-    key: String,
-    value: String,
-}
-
-impl HttpRequest {
-    fn new(buf: Vec<u8>) -> Self {
-        let request = String::from_utf8(buf).unwrap();
-        let mut lines = request.lines().collect::<Vec<_>>().into_iter();
-        let mut status_line = lines.next().unwrap().split_whitespace();
-
-        let mut headers = Vec::new();
-        while let Some(header_line) = lines.next() {
-            if let Some((key, value)) = header_line.split_once(':') {
-                headers.push(Header {
-                    key: key.trim().to_string(),
-                    value: value.trim().to_string(),
-                });
-            }
-        }
-        let body = String::from(lines.collect::<Vec<&str>>().concat());
-
-        HttpRequest {
-            method: status_line.next().unwrap().to_string(),
-            target: status_line.next().unwrap().to_string(),
-            version: status_line.next().unwrap().to_string(),
-            headers: headers.to_vec(),
-            body: Some(body),
-        }
-    }
-}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -57,59 +20,27 @@ fn main() {
 }
 
 fn handle_connection_success(mut stream: std::net::TcpStream) {
-    // let mut buf = [0; 1024];
-    // let _ = stream.peek(&mut buf).expect("failed peaking request");
-    //
-    // let req = parse_http_request(String::from_utf8_lossy(&buf).lines().collect());
-
-    // let mut buf = Vec::<u8>::new();
     let mut buf = [0; 1024];
 
-    let len = stream.read(&mut buf).expect("failed to reat to string");
+    let _ = stream.read(&mut buf).expect("failed to reat to string");
 
     let request = HttpRequest::new(buf.to_vec());
 
     let status = handle_routes(&request.target);
 
-    let binding = [HTTP_VERSION, " ", status, "\r\n\r\n"].concat();
-    let response = binding.as_bytes();
+    let response = HttpResponse::new(status, vec![], None);
 
     println!("accepted new connection");
-    let _ = stream.write(response);
+    let _ = stream.write(&response.as_bytes());
 }
 
 fn handle_connection_error(error: impl Error) {
     println!("error: {}", error);
 }
 
-fn handle_routes(target: &String) -> &str {
+fn handle_routes(target: &String) -> HttpStatus {
     match target.as_str() {
-        "/" => "200 OK",
-        _ => "404 Not Found",
+        "/" => HttpStatus::OK,
+        _ => HttpStatus::NotFound,
     }
 }
-
-// fn parse_http_request(request: Vec<u8>) -> HttpRequest {
-//     let req_line = request.to_string();
-//     let req_line_values: Vec<_> = request[0].to_string().split_whitespace();
-//     HttpRequest {
-//         method: req_line[0].to_string(),
-//         target: req_line[1].to_string(),
-//         version: req_line[2].to_string(),
-//     }
-// }
-
-// fn parse_headers(request: Vec<&str>) -> Vec<(&str, &str)> {
-//     request
-//         .iter()
-//         .skip(2)
-//         .take_while(|line| line.is_empty())
-//         .filter_map(|line| {
-//             if let Some((key, value)) = line.split_once(':') {
-//                 Some((key.trim(), value.trim()))
-//             } else {
-//                 None
-//             }
-//         })
-//         .collect()
-// }
